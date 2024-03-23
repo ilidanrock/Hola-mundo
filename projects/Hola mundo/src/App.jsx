d65aa86ce6d789334d1e2b727253e80abcd96acb
Luis Rivera <rluis747@gmail.com>
2024-03-23 12:04:38 -0500

    inital commit

---
+++ projects/Hola mundo/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TwitterFollowCard from './TwitterFollowCard';
+
+export default function App() {
+  return (
+    <>
+        <TwitterFollowCard nickName={"Ilidanrock"} userName='Luis Rivera' />
+    <TwitterFollowCard nickName={"Ilidanrock"} userName='Luis Rivera' />
+    </>
+
+  )
+}
---
+++ projects/Hola mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,33 @@
+import profile from "./assets/profileimg.jpg"
+import PropTypes from 'prop-types';
+
+export default function TwitterFollowCard({
+    nickName , 
+    userName
+}) {
+  return (
+    <article className="tw-followCard">
+        <header className="tw-followCard-header">
+            <img className="tw-followCard-avatar" alt= "User image profile"  src={profile} />
+            <div className="tw-followCard-info">
+                <strong>{ userName }</strong>
+                <span
+                className="tw-followCard-infoUserName"
+                >{nickName}</span>
+            </div>
+        </header>
+        <aside>
+            <button
+            className="tw-followCard-button"
+            >
+                Seguir
+            </button>
+        </aside>
+    </article>
+  )
+}
+
+TwitterFollowCard.propTypes = {
+    nickName: PropTypes.string.isRequired,
+    userName: PropTypes.string.isRequired
+  };
