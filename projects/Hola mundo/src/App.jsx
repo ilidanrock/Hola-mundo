04825beb11b46b9fa25bef637697594115f00fd1
Luis Rivera <rluis747@gmail.com>
2024-03-23 17:15:36 -0500

    prettier

--- projects/Hola mundo/src/App.jsx
+++ projects/Hola mundo/src/App.jsx
@@ -1,13 +1,11 @@
-
-import './App.css';
-import TwitterFollowCard from './TwitterFollowCard';
+import "./App.css";
+import TwitterFollowCard from "./TwitterFollowCard";
 
 export default function App() {
   return (
     <>
-        <TwitterFollowCard nickName={"Ilidanrock"} userName='Luis Rivera' />
-    <TwitterFollowCard nickName={"Ilidanrock"} userName='Luis Rivera' />
+      <TwitterFollowCard nickName={"Ilidanrock"} userName="Luis Rivera" />
+      <TwitterFollowCard nickName={"Ilidanrock"} userName="Luis Rivera" />
     </>
-
-  )
+  );
 }
--- projects/Hola mundo/src/TwitterFollowCard.jsx
+++ projects/Hola mundo/src/TwitterFollowCard.jsx
@@ -1,33 +1,28 @@
-import profile from "./assets/profileimg.jpg"
-import PropTypes from 'prop-types';
+import profile from "./assets/profileimg.jpg";
+import PropTypes from "prop-types";
 
-export default function TwitterFollowCard({
-    nickName , 
-    userName
-}) {
+export default function TwitterFollowCard({ nickName, userName }) {
   return (
     <article className="tw-followCard">
-        <header className="tw-followCard-header">
-            <img className="tw-followCard-avatar" alt= "User image profile"  src={profile} />
-            <div className="tw-followCard-info">
-                <strong>{ userName }</strong>
-                <span
-                className="tw-followCard-infoUserName"
-                >{nickName}</span>
-            </div>
-        </header>
-        <aside>
-            <button
-            className="tw-followCard-button"
-            >
-                Seguir
-            </button>
-        </aside>
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          alt="User image profile"
+          src={profile}
+        />
+        <div className="tw-followCard-info">
+          <strong>{userName}</strong>
+          <span className="tw-followCard-infoUserName">{nickName}</span>
+        </div>
+      </header>
+      <aside>
+        <button className="tw-followCard-button">Seguir</button>
+      </aside>
     </article>
-  )
+  );
 }
 
 TwitterFollowCard.propTypes = {
-    nickName: PropTypes.string.isRequired,
-    userName: PropTypes.string.isRequired
-  };
+  nickName: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+};
